6367d963a54ec4bc708864f3095f2ea253f5c3d1
Amir Sohi <amirasohi@gmail.com>
2023-11-29 10:24:44 +0000

    remove comment from code

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000
 
-// whenever the path has /static serve the static directory as per usual test
+// whenever the path has /static serve the static directory as per usual
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
 app.get("/*", (req, res) => {
